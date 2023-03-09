71b152bb097a5c83a48c8b084c992866a7d3162d
Moritz Binder <72614975+w1pee@users.noreply.github.com>
2023-03-09 22:17:40 +0100

    update

--- main.cpp
+++ main.cpp
@@ -4,16 +4,13 @@ using namespace std;
 class game
 {
     public: 
-
         int FieldSize;
         int Field[81];
-        
         game(int size){
             FieldSize = size;
             for (int i = 0; i < 81; i++){Field[i] = 0;}
         }
         int interpret(string input){
-
             int n1 = 0;
             int n2 = 0;
             char chars[9] = {'a','b','c','d','e','f','g','h','i'};
@@ -24,7 +21,6 @@ class game
                 }
             }
             n2 = stringTOint(input);
-            // cout << n1 << "|" << n2 << "|";
             return (n1 * FieldSize) + n2;
         }
         int quad(int num, int q){
@@ -35,14 +31,11 @@ class game
             }
             return number;
         }
-
         int stringTOint(string text){
-    
             int num = 0;
             char  nums[10] = {'0','1','2','3','4','5','6','7','8','9'};
             int k = 0;
             bool test = false;
-
             for (int i = text.length()-1; i >= 0; i--)
             {
                 cout << endl;
@@ -61,14 +54,11 @@ class game
         }
 };
 int main(){
-
     game MyGame(3);
-
     string input;
     cout << "input: ";
     cin >> input;
     cout << MyGame.interpret(input);
-    
     return 0;
 }
 
